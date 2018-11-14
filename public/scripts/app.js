c8cd38ed7e320890cff33deb4b631104893f3747
DanDitomaso <dan.ditomaso@gmail.com>
2018-11-13 23:14:14 -0500

    Added data.json file to project

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,28 @@ const myApp = {
     wrongAnswer: 0,
 };
 
+// read in question/answer JSON object data
 
+// Function found on the MDN website.  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+//Display questions to page
+
+// Add Event Listender for answer clicks
+
+// Check for winning answer + add to myapp.Correct/Wrong object
 
 myApp.init = function () {
+    $.getJSON('../public/assets/data.json')
+        .then((res) => {
+            console.log(res)
 
+        }
+        )
 }
 
 
